eb5a5e86bf5eb306581ee4429f9797fb520ab3cd
divyanshutewari17 <51994346+divyanshutewari17@users.noreply.github.com>
2020-07-15 16:40:36 +0530

    CUTBOARD

---
+++ cutboard.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<cmath>
+#include<cstring>
+#include<vector> 
+#include<algorithm>
+#define ll long long
+#define lli long long int
+using namespace std;
+
+int main() {
+    int t,n,m;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n>>m;
+        cout<<(n-1)*(m-1)<<"\n";
+    }
+}
