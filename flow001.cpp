76abf9ff909e0b764193c25c8abd93bb40c6704b
divyanshutewari17 <51994346+divyanshutewari17@users.noreply.github.com>
2020-07-15 16:36:47 +0530

    FLOW001

---
+++ flow001.cpp
@@ -0,0 +1,20 @@
+// bits/stdc++.h works in linux. 
+// It loads most of the libraries of C++ required.
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main() {
+	// Read the number of test cases.
+    int t,a,b;
+    cin>>t;
+    for(int i =0;i<t;i++)
+    {
+        cin>>a>>b;
+        cout<<a+b<<"\n";
+    }
+    
+	
+
+	return 0;
+}
